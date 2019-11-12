c42a868d79a66fd4cacf535cf9a4605e96a943a3
artem <artemfap@yandex.ru>
2019-11-12 23:07:20 +0300

    'commit7'

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -3,15 +3,19 @@ $(document).ready(function () {
     $('.portfolio__btn[filter="all"]').click(function () {
         $('.portfolio__btn').attr('val', 'off');
         $(this).attr('val', 'on');
-        $('.portfolio__images > div').show(300);
+        $('.portfolio__btn').removeClass('focused');
+        $(this).addClass('focused');
+        $('.portfolio__images > div').show(100);
     });
     // web design
     $('.portfolio__btn[filter="wd"]').click(function () {
         if($(this).attr('val')==='off') {
             $('.portfolio__btn').attr('val', 'off');
             $(this).attr('val', 'on');
-            $('.portfolio__images > div').hide(300);
-            $('.portfolio__images > div[filter="wd"]').show(300);
+            $('.portfolio__btn').removeClass('focused');
+            $(this).addClass('focused');
+            $('.portfolio__images > div').hide(100);
+            $('.portfolio__images > div[filter="wd"]').show(100);
         }
     });
     // Ui/Ux design
@@ -19,8 +23,10 @@ $(document).ready(function () {
         if($(this).attr('val')==='off') {
             $('.portfolio__btn').attr('val', 'off');
             $(this).attr('val', 'on');
-            $('.portfolio__images > div').hide(300);
-            $('.portfolio__images > div[filter="ud"]').show(300);
+            $('.portfolio__btn').removeClass('focused');
+            $(this).addClass('focused');
+            $('.portfolio__images > div').hide(100);
+            $('.portfolio__images > div[filter="ud"]').show(100);
         }
     });
     // Mockups
@@ -28,6 +34,8 @@ $(document).ready(function () {
         if($(this).attr('val')==='off') {
             $('.portfolio__btn').attr('val', 'off');
             $(this).attr('val', 'on');
+            $('.portfolio__btn').removeClass('focused');
+            $(this).addClass('focused');
             $('.portfolio__images > div').hide(300);
             $('.portfolio__images > div[filter="moc"]').show(300);
         }
